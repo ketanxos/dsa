ff676c3df43288bb0e94e80b90dbe5dd08f32da5
Ketan Kumar <ketan.kumar08032002@gmail.com>
2025-03-23 00:36:46 +0530

    Update p1.cpp

--- cpp/leetcode/p1.cpp
+++ cpp/leetcode/p1.cpp
@@ -1,11 +1,17 @@
-class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
-        num_map = {}
-        
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in num_map:
-                return [num_map[complement], i]
-            num_map[num] = i
-        return []
-        
+#include <unordered_map>
+#include <vector>
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> numMap;
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (numMap.count(complement)) {
+                return {numMap[complement], i};
+            }
+            numMap[nums[i]] = i;
+        }
+        return {};
+    }
+};
