efdafd63d96859e8cca0943ab96a0e1f6f10a481
Ketan Kumar <ketan.kumar08032002@gmail.com>
2025-03-23 00:34:25 +0530

    Create p2.cpp

---
+++ cpp/leetcode/p2.cpp
@@ -0,0 +1,21 @@
+function addTwoNumbers(l1, l2) {
+    const dummy = new ListNode(0);
+    let current = dummy;
+    let carry = 0;
+    
+    while (l1 || l2 || carry) {
+        let sum = carry;
+        if (l1) {
+            sum += l1.val;
+            l1 = l1.next;
+        }
+        if (l2) {
+            sum += l2.val;
+            l2 = l2.next;
+        }
+        carry = Math.floor(sum / 10);
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+    }
+    return dummy.next;
+}
