ba7046e9485e3bccde712f9ef9d465883028a0dd
Ketan Kumar <ketan.kumar08032002@gmail.com>
2025-03-23 00:37:53 +0530

    Update p2.cpp

--- cpp/leetcode/p2.cpp
+++ cpp/leetcode/p2.cpp
@@ -1,21 +1,19 @@
-function addTwoNumbers(l1, l2) {
-    const dummy = new ListNode(0);
-    let current = dummy;
-    let carry = 0;
-    
-    while (l1 || l2 || carry) {
-        let sum = carry;
-        if (l1) {
-            sum += l1.val;
-            l1 = l1.next;
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        ListNode* current = &dummy;
+        int carry = 0;
+        
+        while (l1 || l2 || carry) {
+            int sum = carry;
+            if (l1) sum += l1->val, l1 = l1->next;
+            if (l2) sum += l2->val, l2 = l2->next;
+            
+            carry = sum / 10;
+            current = current->next = new ListNode(sum % 10);
         }
-        if (l2) {
-            sum += l2.val;
-            l2 = l2.next;
-        }
-        carry = Math.floor(sum / 10);
-        current.next = new ListNode(sum % 10);
-        current = current.next;
+        
+        return dummy.next;
     }
-    return dummy.next;
-}
+};
